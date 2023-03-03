eef5ed61c4d9e2024c6de355e182a235e1ffc942
dallensmith1@mail.weber.edu <Dallen Smith>
2023-03-03 11:09:56 -0700

    still broken need help

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-    def sort
-        @quotes = Quote.all
-    end
+  def sort
+    @quotes = Quote.all
+  end
     
   def sort_by_saying_length
-        @quotes = Quote.all.order('saying_length')
-        render :sort   
+    @quotes = Quote.all.order('saying_length')
+    render :sort   
   end
   def sort_by_said_by_length
     @quotes = Quote.all.order('said_by_length')
--- db/migrate/20230302183648_create_quotes.rb
+++ db/migrate/20230302183648_create_quotes.rb
@@ -1,10 +1,10 @@
 class CreateQuotes < ActiveRecord::Migration[7.0]
   def change
     create_table :quotes do |t|
-      t.string :saying
-      t.string :said_by
+      t.string :saying_length
+      t.string :said_by_length
       t.integer :votes
-      t.integer :year
+      t.integer :year_length
       t.text :notes
 
       t.timestamps
